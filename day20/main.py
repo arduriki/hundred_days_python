2f9de85843f7eeba09326f119887b16515c3eea3
Jordi Ardura <jordi.ardura.professional@gmail.com>
2024-04-11 12:55:01 +0200

    snake created

---
+++ day20/main.py
@@ -0,0 +1,45 @@
+from turtle import Screen, Turtle
+from snake import Snake
+import time
+
+# MARK: Set up screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up(), "Up")
+screen.onkey(snake.down(), "Down")
+screen.onkey(snake.left(), "Left")
+screen.onkey(snake.right(), "Right")
+
+# MARK: Move the Snake
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    
+    snake.move()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
